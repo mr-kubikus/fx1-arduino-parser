f4138fcc92355a44cbacdfda8b6990a603f0643b
mr-kubikus <mr-kubikus@users.noreply.github.com>
2015-05-20 16:55:51 +0300

    Create common.h

---
+++ fx1sample/common.h
@@ -0,0 +1,21 @@
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+//  Declaration of data types
+
+typedef signed char         CHAR8;
+typedef unsigned char       UCHAR8;
+
+typedef unsigned char       BOOL8;        //  Boolean variable (should be TRUE or FALSE)
+typedef unsigned short      BOOL16;
+typedef unsigned long       BOOL32;
+
+typedef signed char         INT8;
+typedef short               INT16;
+typedef signed long         INT32;
+
+typedef unsigned char       UINT8;
+typedef unsigned short      UINT16;
+typedef unsigned long       UINT32;
+
+#endif
